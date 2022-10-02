055737c15cc79f8632ec7491dff9047900165123
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2022-10-02 12:02:29 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * PACKAGE_NAME
+ * Created by Main
+ * Date 10/2/2022 - 12:15 AM
+ * Description: ...
+ */
+public class Main {
+    /**
+     * main fucntion
+     * @param argv parameters from terminal
+     * */
+    public static void main(String[] args) {
+
+        System.out.println("Hello Phan Huy");
+        
+    }
+}
