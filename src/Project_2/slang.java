f6ac36aa8c5222da8cb8e3f343237209add96fdd
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2022-11-28 22:48:04 +0700

    Create new object slang & constructor function, getter, setter. [slang-1]

--- src/Project_2/slang.java
+++ src/Project_2/slang.java
@@ -9,4 +9,45 @@ package Project_2;
  * Description: ...
  */
 public class slang {
+    //Declare attribute for class slang
+    String slang;
+    String meaning;
+
+    //Default constructor of slang
+    public slang(){
+        this.slang = null;
+        this.meaning = null;
+    }
+
+    //Constructor with parameters
+    public slang(String slang, String meaning) {
+        this.slang = slang;
+        this.meaning = meaning;
+    }
+
+    //Copy constructor with parameter is a slang obj
+    public slang(slang sl){
+        this.slang = sl.slang;
+        this.meaning = sl.meaning;
+    }
+
+    //getter of slang attribute
+    public String getSlang(){
+        return this.slang;
+    }
+
+    //getter of meaning attribute
+    public String getMeaning(){
+        return this.meaning;
+    }
+
+    //setter of slang attribute & meaning attribute
+
+    public void setSlang(String slang){
+        this.slang = slang;
+    }
+
+    public void setMeaning(String meaning){
+        this.meaning = meaning;
+    }
 }
