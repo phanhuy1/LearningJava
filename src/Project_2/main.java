06b75775b2dbbdca9b931ef1f70b7a7f00c74585
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2022-11-28 23:14:10 +0700

    Update edit main file with adding read, search & search for meaning functions  - [slang-03]

--- src/Project_2/main.java
+++ src/Project_2/main.java
@@ -1,12 +1,58 @@
 package Project_2;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * IntelliJ IDEA
  * User: gohuy
  * Package: Project_2
  * Created by Phan Huy
- * Date 11/27/2022 - 9:18 PM
  * Description: ...
  */
 public class main {
+    static HashMap<String,String> hashMap = new HashMap<String,String>();
+    static ArrayList<slang> reset = new ArrayList<slang>();
+    static ArrayList<slang> list = new ArrayList<slang>();
+    static Scanner scan = new Scanner(System.in);
+
+    // Read Slang Word from file
+    public static void ReadSlang(String filename) throws IOException {
+        list.clear();
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = reader.readLine();
+        // Read line from input.txt file
+        while ((line = reader.readLine()) != null) {
+            // Solve the string
+            String[] info = line.split("`");
+            slang a = new slang(info[0], info[1]);
+            hashMap.put(info[0],info[1]);
+            list.add(a);
+            reset.add(a);
+        }
+        reader.close();
+    }
+
+    // Search Slang word
+    public static String SearchSlangWord(String slangWord) {
+        for (Map.Entry<String, String> set :
+                hashMap.entrySet()) {
+            if(set.getKey().equalsIgnoreCase(slangWord)){
+                return set.getValue();
+            }
+        }
+        return null;
+    }
+
+    // Search Meaning
+    public static ArrayList<String> SearchMeaning(String input){
+        ArrayList<String> slang = new ArrayList<String>();
+        for (Map.Entry<String, String> set :
+                hashMap.entrySet()) {
+            if(set.getValue().contains(input)){
+                slang.add(set.getKey());
+            }
+        }
+        return slang;
+    }
 }
