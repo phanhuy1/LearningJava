82cea3b1039714a701ad8f00e5e95ecb3d4e831d
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2022-11-29 16:13:24 +0700

    Update - [slang-04]
    Update FrameUI add function - [slang- 04]

--- src/Project_2/main.java
+++ src/Project_2/main.java
@@ -16,7 +16,7 @@ public class main {
     static ArrayList<slang> list = new ArrayList<slang>();
     static Scanner scan = new Scanner(System.in);
 
-    // Read Slang Word from file
+    // Read slang Word from file
     public static void ReadSlang(String filename) throws IOException {
         list.clear();
         BufferedReader reader = new BufferedReader(new FileReader(filename));
@@ -24,9 +24,16 @@ public class main {
         // Read line from input.txt file
         while ((line = reader.readLine()) != null) {
             // Solve the string
-            String[] info = line.split("`");
-            slang a = new slang(info[0], info[1]);
-            hashMap.put(info[0],info[1]);
+            slang a;
+            if(line.contains("`")) {
+                String[] info = line.split("`");
+                a = new slang(info[0], info[1]);
+                hashMap.put(info[0], info[1]);
+            }
+            else {
+                a = new slang("", line);
+                hashMap.put("", line);
+            }
             list.add(a);
             reset.add(a);
         }
@@ -54,5 +61,70 @@ public class main {
             }
         }
         return slang;
+    }
+    // Edit Slang
+    public static void EditSlang(slang edit){
+        if(hashMap.containsKey(edit.getSlang()))
+            hashMap.replace(edit.getSlang(),edit.getMeaning());
+        for(slang i : list)
+            if(i.getSlang().equalsIgnoreCase(edit.getSlang())) {
+                i.setMeaning(edit.getMeaning());
+                break;
+            }
+    }
+
+    // Check exist
+    public static boolean CheckSlang(slang a) {
+        for (Map.Entry<String, String> set : hashMap.entrySet()) {
+            if (set.getKey().equalsIgnoreCase(a.getSlang()))
+                return true;
+        }
+        return false;
+    }
+    // Add Slang
+    public static void AddSlang(slang add){
+        list.add(add);
+        hashMap.put(add.getSlang(),add.getMeaning());
+    }
+
+    // Remove
+    public static void RemoveSlang(slang remove){
+        if(!CheckSlang(remove))
+            return;
+        int i = 0;
+        while(i < list.size()){
+            if(list.get(i).getSlang().equalsIgnoreCase(remove.getSlang()))
+                break;
+            else
+                i++;
+        }
+        list.remove(i);
+        hashMap.remove(remove.getSlang(),remove.getMeaning());
+    }
+
+    // Reset
+    public static void ResetSlang() throws  IOException{
+        ReadSlang("D:\\slang.txt");
+    }
+
+    // Random
+    public static slang RandomSlang(){
+        int index = new Random().nextInt(list.size());
+        return list.get(index);
+    }
+
+    // Get list
+    public static ArrayList<slang> GetSlangList(){
+        return list;
+    }
+
+    // Save file
+    public static void WriteSlang(String filename) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write("Slag`Meaning" + "\n");
+        for(slang i : list){
+            writer.write(i.getSlang() + '`' + i.getMeaning() + "\n");
+        }
+        writer.close();
     }
 }
